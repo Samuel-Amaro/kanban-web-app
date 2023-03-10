dd6cb5c8f41c7d63ee6849018db293c24ff398fb
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-03-10 11:31:19 -0300

    [feat]: componentes de icon svg é adicionando elementos html no componente header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,61 @@
 import Button from "../Button";
 import Heading from "../Heading";
+import ChevronDown from "../Icons/ChevronDown";
+import LogoMobile from "../Icons/LogoMobile";
 import VerticalEllipsis from "../Icons/VerticalEllipsis";
 
+//TODO: ADICIONAR INTERATIVIDADE VIA MOUSE E TECLADO
+//TODO: ADICIONAR LOGICA DE ESTADO
+
 export default function Header() {
-    return(
-        <header className="header">
-            <Heading level={1} className="header__name-board">Name Board</Heading>
-            <div className="header__container">
-                <Button type="button" size="l" variant="primary">+ Add New Task</Button>
-                <VerticalEllipsis />
-            </div>
-        </header>
+    return (
+      <header className="header">
+        {/*mostrar somente a partir de dispositivos como tablets e desktops*/}
+        <Heading level={1} className="header__name-board">
+          Name Board
+        </Heading>
+        {/*mostrar somente em layout mobile*/}
+        <div className="header__group">
+          <LogoMobile />
+          <div className="header__container">
+            <Heading level={1} className="header__name-board">
+              Name Board
+            </Heading>
+            <ChevronDown />
+          </div>
+        </div>
+        {/*--------*/}
+        <div className="header__container">
+          <Button type="button" size="l" variant="primary" title="Add New Task">
+            + Add New Task
+          </Button>
+          {/*button menu to actions board*/}
+          <div className="header__menu-button-board">
+            <button
+              type="button"
+              className="header__btn-board"
+              title="Show options for current board"
+              id="menubutton1"
+              aria-haspopup="true"
+              aria-controls="menu1"
+            >
+              <VerticalEllipsis />
+            </button>
+            <ul
+              id="menu1"
+              role="menu"
+              aria-labelledby="menubutton1"
+              className="header__menu-board"
+            >
+              <li role="menuitem" className="header__menu-item">
+                Edit Board
+              </li>
+              <li role="menuitem" className="header__menu-item">
+                Delete Board
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
     );
 }
---
+++ src/components/Icons/ChevronDown.tsx
@@ -0,0 +1,7 @@
+export default function ChevronDown() {
+    return (
+      <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
+        <path stroke="#635FC7" strokeWidth="2" fill="none" d="m1 1 4 4 4-4" />
+      </svg>
+    );
+}
